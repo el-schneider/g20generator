0028d5beecf3ffa15ab4c2f5b1f5e6131ad54783
Jonas List <mail@jonaslist.de>
2020-10-17 11:07:21 +0200

    building ...

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ canvas-sketch src/index.js --dir docs --build
 
 #this is only necessary for github pages
 sed -i 's/src\/assets/g20generator\/src\/assets/g' docs/index.js
+
+#deploying
+git commit -a -m "building ..."
+git push
